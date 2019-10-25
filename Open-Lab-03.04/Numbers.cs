7bfb7455fc324d67f917dabc1fe36b24086a558f
Erik Zemčík <zemcik.erik@gmail.com>
2019-10-25 19:45:11 +0200

    Created tests

---
+++ Open-Lab-03.04/Numbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_03._04
+{
+    public class Numbers
+    {
+        public string EvenOrOdd(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-03.04/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Open_Lab_03._04
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private Numbers numbers;
+
+        private const int RandMaxNum = 1000000;
+        private const int RandSeed = 304304304;
+        private const int RandTestCasesCount = 97;
+
+        [OneTimeSetUp]
+        public void Init() => numbers = new Numbers();
+
+        [TestCase(3, "odd")]
+        [TestCase(19, "odd")]
+        [TestCase(146, "even")]
+        [TestCaseSource(nameof(GetRandom))]
+        public void EvenOrOddTest(int number, string expectedOutput) =>
+            Assert.That(numbers.EvenOrOdd(number), Is.EqualTo(expectedOutput));
+
+        private static IEnumerable GetRandom()
+        {
+            var random = new Random(RandSeed);
+
+            for (var i = 0; i < RandTestCasesCount; i++)
+            {
+                var num = random.Next(RandMaxNum + 1);
+                yield return new TestCaseData(num, num % 2 == 0 ? "even" : "odd");
+            }
+        }
+
+    }
+}
